daca311a57f60e554e2dca805832f056c42c3316
Claire Gizard <cgizard>
2022-01-19 16:01:04 +0100

    Fix lint rule

--- vector/src/main/java/fr/gouv/tchap/core/utils/TchapUtils.kt
+++ vector/src/main/java/fr/gouv/tchap/core/utils/TchapUtils.kt
@@ -207,12 +207,6 @@ object TchapUtils {
     private const val DISPLAY_NAME_SECOND_DELIMITER = "]"
     private const val DEFAULT_EMPTY_STRING = ""
 
-    enum class ConfigurationVariant {
-        PREPROD,
-        AGENT,
-        SECURE
-    }
-
     /**
      * Get the current configuration variant
      * @return the ConfigurationVariant
@@ -225,4 +219,10 @@ object TchapUtils {
             else      -> ConfigurationVariant.SECURE
         }
     }
+}
+
+enum class ConfigurationVariant {
+    PREPROD,
+    AGENT,
+    SECURE
 }
