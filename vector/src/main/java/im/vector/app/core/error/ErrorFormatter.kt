VoyageClient/Voyage

Use Throwable.isLimitExceededError extension

vector/src/main/java/im/vector/app/core/error/ErrorFormatter.kt
@@ -24,6 +24,7 @@ import org.matrix.android.sdk.api.failure.Failure
 import org.matrix.android.sdk.api.failure.MatrixError
 import org.matrix.android.sdk.api.failure.MatrixIdFailure
 import org.matrix.android.sdk.api.failure.isInvalidPassword
+import org.matrix.android.sdk.api.failure.isLimitExceededError
 import org.matrix.android.sdk.api.session.identity.IdentityServiceError
 import java.net.HttpURLConnection
 import java.net.SocketTimeoutException
@@ -58,53 +59,53 @@ class DefaultErrorFormatter @Inject constructor(
             }
             is Failure.ServerError                 -> {
                 when {
-                    throwable.error.code == MatrixError.M_CONSENT_NOT_GIVEN       -> {
+                    throwable.error.code == MatrixError.M_CONSENT_NOT_GIVEN            -> {
                         // Special case for terms and conditions
                         stringProvider.getString(R.string.error_terms_not_accepted)
                     }
-                    throwable.isInvalidPassword()                                 -> {
+                    throwable.isInvalidPassword()                                      -> {
                         stringProvider.getString(R.string.auth_invalid_login_param)
                     }
-                    throwable.error.code == MatrixError.M_USER_IN_USE             -> {
+                    throwable.error.code == MatrixError.M_USER_IN_USE                  -> {
                         stringProvider.getString(R.string.login_signup_error_user_in_use)
                     }
-                    throwable.error.code == MatrixError.M_BAD_JSON                -> {
+                    throwable.error.code == MatrixError.M_BAD_JSON                     -> {
                         stringProvider.getString(R.string.login_error_bad_json)
                     }
-                    throwable.error.code == MatrixError.M_NOT_JSON                -> {
+                    throwable.error.code == MatrixError.M_NOT_JSON                     -> {
                         stringProvider.getString(R.string.login_error_not_json)
                     }
-                    throwable.error.code == MatrixError.M_THREEPID_DENIED         -> {
+                    throwable.error.code == MatrixError.M_THREEPID_DENIED              -> {
                         stringProvider.getString(R.string.login_error_threepid_denied)
                     }
-                    throwable.error.code == MatrixError.M_LIMIT_EXCEEDED          -> {
+                    throwable.isLimitExceededError()                                   -> {
                         limitExceededError(throwable.error)
                     }
-                    throwable.error.code == MatrixError.M_TOO_LARGE               -> {
+                    throwable.error.code == MatrixError.M_TOO_LARGE                    -> {
                         stringProvider.getString(R.string.error_file_too_big_simple)
                     }
-                    throwable.error.code == MatrixError.M_THREEPID_NOT_FOUND      -> {
+                    throwable.error.code == MatrixError.M_THREEPID_NOT_FOUND           -> {
                         stringProvider.getString(R.string.login_reset_password_error_not_found)
                     }
-                    throwable.error.code == MatrixError.M_USER_DEACTIVATED        -> {
+                    throwable.error.code == MatrixError.M_USER_DEACTIVATED             -> {
                         stringProvider.getString(R.string.auth_invalid_login_deactivated_account)
                     }
                     throwable.error.code == MatrixError.M_THREEPID_IN_USE &&
-                            throwable.error.message == "Email is already in use"  -> {
+                            throwable.error.message == "Email is already in use"       -> {
                         stringProvider.getString(R.string.account_email_already_used_error)
                     }
                     throwable.error.code == MatrixError.M_THREEPID_IN_USE &&
-                            throwable.error.message == "MSISDN is already in use" -> {
+                            throwable.error.message == "MSISDN is already in use"      -> {
                         stringProvider.getString(R.string.account_phone_number_already_used_error)
                     }
-                    throwable.error.code == MatrixError.M_THREEPID_AUTH_FAILED    -> {
+                    throwable.error.code == MatrixError.M_THREEPID_AUTH_FAILED         -> {
                         stringProvider.getString(R.string.error_threepid_auth_failed)
                     }
                     throwable.error.code == MatrixError.M_UNKNOWN &&
                             throwable.error.message == "Not allowed to join this room" -> {
                         stringProvider.getString(R.string.room_error_access_unauthorized)
                     }
-                    else                                                          -> {
+                    else                                                               -> {
                         throwable.error.message.takeIf { it.isNotEmpty() }
                                 ?: throwable.error.code.takeIf { it.isNotEmpty() }
                     }

tchapgouv/tchap-android

(cherry picked from commit 83c961e2552b8d7ee5af5ae48fe261c0a5496e81)

vector/src/main/java/im/vector/app/core/error/ErrorFormatter.kt
@@ -24,6 +24,7 @@ import org.matrix.android.sdk.api.failure.Failure
 import org.matrix.android.sdk.api.failure.MatrixError
 import org.matrix.android.sdk.api.failure.MatrixIdFailure
 import org.matrix.android.sdk.api.failure.isInvalidPassword
+import org.matrix.android.sdk.api.failure.isLimitExceededError
 import org.matrix.android.sdk.api.session.contentscanner.ScanFailure
 import org.matrix.android.sdk.api.session.identity.IdentityServiceError
 import java.net.HttpURLConnection
@@ -78,7 +79,7 @@ class DefaultErrorFormatter @Inject constructor(
                     throwable.error.code == MatrixError.M_THREEPID_DENIED         -> {
                         stringProvider.getString(R.string.login_error_threepid_denied)
                     }
-                    throwable.error.code == MatrixError.M_LIMIT_EXCEEDED          -> {
+                    throwable.isLimitExceededError()                              -> {
                         limitExceededError(throwable.error)
                     }
                     throwable.error.code == MatrixError.M_TOO_LARGE               -> {
@@ -101,19 +102,19 @@ class DefaultErrorFormatter @Inject constructor(
                     throwable.error.code == MatrixError.M_THREEPID_AUTH_FAILED    -> {
                         stringProvider.getString(R.string.error_threepid_auth_failed)
                     }
-                    throwable.error.code == MatrixError.M_PASSWORD_TOO_SHORT -> {
+                    throwable.error.code == MatrixError.M_PASSWORD_TOO_SHORT      -> {
                         stringProvider.getString(R.string.tchap_register_pwd_too_short)
                     }
-                    throwable.error.code == MatrixError.M_PASSWORD_NO_UPPERCASE -> {
+                    throwable.error.code == MatrixError.M_PASSWORD_NO_UPPERCASE   -> {
                         stringProvider.getString(R.string.tchap_register_pwd_no_uppercase)
                     }
-                    throwable.error.code == MatrixError.M_PASSWORD_NO_DIGIT -> {
+                    throwable.error.code == MatrixError.M_PASSWORD_NO_DIGIT       -> {
                         stringProvider.getString(R.string.tchap_register_pwd_no_digit)
                     }
-                    throwable.error.code == MatrixError.M_PASSWORD_NO_LOWERCASE -> {
+                    throwable.error.code == MatrixError.M_PASSWORD_NO_LOWERCASE   -> {
                         stringProvider.getString(R.string.tchap_register_pwd_no_lowercase)
                     }
-                    throwable.error.code == MatrixError.M_PASSWORD_NO_SYMBOL -> {
+                    throwable.error.code == MatrixError.M_PASSWORD_NO_SYMBOL      -> {
                         stringProvider.getString(R.string.tchap_register_pwd_no_symbol)
                     }
                     else                                                          -> {