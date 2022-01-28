7ac264153540c9bd21e5867c24fd9b358c4dec3f
Claire G <claire.1817@hotmail.fr>
2022-01-28 14:28:08 +0100

    add new script: sign all apk in a directory and hash them

---
+++ tools/release/sign_all_apks.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Get the command line parameters
+PARAM_KEYSTORE_PATH=$1
+PARAM_DIRECTORY=$2
+CHECKSUM_FILE="checksum.txt"
+
+# Sign all the apks in the directory PARAM_DIRECTORY
+for file in ${PARAM_DIRECTORY}/*
+do
+  if [[ ${file: -4} == ".apk" ]]
+  then
+    echo "Signing.... ${file}"
+    sh ./sign_apk.sh "${PARAM_KEYSTORE_PATH}" "${file}"
+  fi
+done
+
+# Rename and Hash all the apks in the directory PARAM_DIRECTORY
+for file in ${PARAM_DIRECTORY}/*
+do
+  if [[ ${file: -4} == ".apk" ]]
+  then
+    # Rename Apk: remove unsigned by signed
+    apkName="$(echo ${file} | sed -e 's/\-unsigned/-signed/')" ;
+    mv "${file}" "${apkName}" ;
+
+    # Hash application with SHA 256
+    echo "Hash SHA 256 on file... ${apkName}"
+    result="$(shasum "-a" "256" ${apkName})"
+
+    # Save hash in file: Checksum.txt
+    resultSplit=(${result})
+    newName="$(echo ${resultSplit[1]} | sed 's/.*\///')"
+    echo "SHA256(${newName})=${resultSplit[0]}" > ${PARAM_DIRECTORY}/${CHECKSUM_FILE}
+  fi
+done
+
+echo "done !! :)"
