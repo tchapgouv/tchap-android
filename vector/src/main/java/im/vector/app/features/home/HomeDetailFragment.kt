37f0644c645f8e6a009bf792f9e02f8466bbf925
Claire G <claire.1817@hotmail.fr>
2022-04-25 12:09:20 +0200

    clean import

--- vector/src/main/java/im/vector/app/features/home/HomeDetailFragment.kt
+++ vector/src/main/java/im/vector/app/features/home/HomeDetailFragment.kt
@@ -23,8 +23,7 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.appcompat.widget.SearchView
-import androidx.core.view.children
+
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
