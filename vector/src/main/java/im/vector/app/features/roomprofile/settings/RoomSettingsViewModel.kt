91c702770a72960c1631f9f897ddcb275a798793
Claire G <claire.1817@hotmail.fr>
2022-08-03 14:49:14 +0200

    Fix bug: remove from forum list is not displayed

--- vector/src/main/java/im/vector/app/features/roomprofile/settings/RoomSettingsViewModel.kt
+++ vector/src/main/java/im/vector/app/features/roomprofile/settings/RoomSettingsViewModel.kt
@@ -199,7 +199,11 @@ class RoomSettingsViewModel @AssistedInject constructor(
                             canChangeRoomAccessRules = powerLevelsHelper.isUserAllowedToSend(
                                     session.myUserId, true,
                                     TchapEventType.STATE_ROOM_ACCESS_RULES
-                            )
+                            ),
+                            canRemoveFromRoomsDirectory = powerLevelsHelper.isUserAllowedToSend(session.myUserId, true, EventType.STATE_ROOM_JOIN_RULES) &&
+                                    powerLevelsHelper.isUserAllowedToSend(session.myUserId, true, EventType.STATE_ROOM_CANONICAL_ALIAS) &&
+                                    powerLevelsHelper.isUserAllowedToSend(session.myUserId, true, EventType.STATE_ROOM_ENCRYPTION) &&
+                                    powerLevelsHelper.isUserAllowedToSend(session.myUserId, true, EventType.STATE_ROOM_HISTORY_VISIBILITY)
                     )
                     setState {
                         copy(actionPermissions = permissions)
