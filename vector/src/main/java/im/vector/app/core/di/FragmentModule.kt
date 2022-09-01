daf4541cd093629821d524f90dc2de138e6637ca
Florian Renaud <florianr@element.io>
2022-09-01 16:21:34 +0200

    Use ftue_auth variant with Tchap welcome view

--- vector/src/main/java/im/vector/app/core/di/FragmentModule.kt
+++ vector/src/main/java/im/vector/app/core/di/FragmentModule.kt
@@ -25,7 +25,6 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import dagger.multibindings.IntoMap
 import fr.gouv.tchap.features.login.TchapLoginFragment
-import fr.gouv.tchap.features.login.TchapWelcomeFragment
 import fr.gouv.tchap.features.login.registration.TchapRegisterFragment
 import fr.gouv.tchap.features.login.registration.TchapRegisterWaitForEmailFragment
 import fr.gouv.tchap.features.roomprofile.settings.linkaccess.TchapRoomLinkAccessFragment
@@ -959,11 +958,6 @@ interface FragmentModule {
     @IntoMap
     @FragmentKey(RoomDevToolSendFormFragment::class)
     fun bindRoomDevToolSendFormFragment(fragment: RoomDevToolSendFormFragment): Fragment
-
-    @Binds
-    @IntoMap
-    @FragmentKey(TchapWelcomeFragment::class)
-    fun bindTchapWelcomeFragment(fragment: TchapWelcomeFragment): Fragment
 
     @Binds
     @IntoMap
