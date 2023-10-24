33c5a3b8b31a7dc81b69585eaa57ca097b2e8da0
yostyle <y.pintas@gmail.com>
2023-10-24 21:45:29 +0200

    Fix lint

--- vector/src/main/java/im/vector/app/features/home/room/detail/timeline/factory/EncryptedItemFactory.kt
+++ vector/src/main/java/im/vector/app/features/home/room/detail/timeline/factory/EncryptedItemFactory.kt
@@ -137,6 +137,7 @@ class EncryptedItemFactory @Inject constructor(
     }
 
     companion object {
-        private const val TCHAP_FAQ_UTD_URL = "https://aide.tchap.beta.gouv.fr/fr/article/dechiffrement-impossible-de-mes-messages-comment-y-remedier-iphone-xotgv1"
+        private const val TCHAP_FAQ_UTD_URL =
+                "https://aide.tchap.beta.gouv.fr/fr/article/dechiffrement-impossible-de-mes-messages-comment-y-remedier-iphone-xotgv1"
     }
 }
