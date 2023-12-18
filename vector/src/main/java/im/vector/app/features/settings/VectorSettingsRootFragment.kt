fbb8205701386d5bdb271f43c8227fd1766de8b8
Yoan Pintas <yoan@pintas.bzh>
2023-12-18 10:21:09 +0000

    Une surprise vous attend dans cette version (#994)

--- vector/src/main/java/im/vector/app/features/settings/VectorSettingsRootFragment.kt
+++ vector/src/main/java/im/vector/app/features/settings/VectorSettingsRootFragment.kt
@@ -16,14 +16,18 @@
 
 package im.vector.app.features.settings
 
+import android.content.Context
 import android.os.Bundle
 import androidx.preference.Preference
 import dagger.hilt.android.AndroidEntryPoint
 import im.vector.app.R
+import im.vector.app.core.extensions.singletonEntryPoint
 import im.vector.app.core.preference.VectorPreference
 import im.vector.app.core.utils.FirstThrottler
 import im.vector.app.core.utils.openUrlInChromeCustomTab
 import im.vector.app.features.analytics.plan.MobileScreen
+import im.vector.app.features.navigation.Navigator
+import java.util.Calendar
 
 @AndroidEntryPoint
 class VectorSettingsRootFragment :
@@ -33,6 +37,14 @@ class VectorSettingsRootFragment :
     override val preferenceXmlRes = R.xml.vector_settings_root
 
     private val firstThrottler = FirstThrottler(1000)
+
+    private lateinit var navigator: Navigator
+
+    override fun onAttach(context: Context) {
+        val singletonEntryPoint = context.singletonEntryPoint()
+        navigator = singletonEntryPoint.navigator()
+        super.onAttach(context)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,6 +61,19 @@ class VectorSettingsRootFragment :
                 openUrlInChromeCustomTab(requireContext(), null, VectorSettingsUrls.HELP)
             }
             false
+        }
+
+        // Tchap: Manage Christmas entry
+        if (Calendar.getInstance().before(Calendar.getInstance().apply { set(2024, 1, 10) })) {
+            findPreference<VectorPreference>(VectorPreferences.TCHAP_SETTINGS_CHRISTMAS_PREFERENCE_KEY)!!.let {
+                it.isVisible = true
+                it.onPreferenceClickListener = Preference.OnPreferenceClickListener {
+                    if (firstThrottler.canHandle() is FirstThrottler.CanHandlerResult.Yes) {
+                        navigator.openRoom(requireContext(), "!cDKdQyXHeWBEaKDWWV:agent.dinum.tchap.gouv.fr", null)
+                    }
+                    false
+                }
+            }
         }
     }
 
