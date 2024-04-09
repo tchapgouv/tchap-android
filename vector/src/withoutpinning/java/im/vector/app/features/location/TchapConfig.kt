ef05fef85168a89f46a38e5894ad1a8f35e2fb56
yostyle <y.pintas@gmail.com>
2024-04-09 15:28:51 +0200

    Use openmaptiles.geo.data.gouv.fr on fdroid and ign on gplay for map tiles.

--- vector/src/withoutpinning/java/im/vector/app/features/location/TchapConfig.kt
+++ vector/src/withoutpinning/java/im/vector/app/features/location/TchapConfig.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 New Vector Ltd
+ * Copyright (c) 2024 New Vector Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,14 +16,5 @@
 
 package im.vector.app.features.location
 
-// Tchap: Tiles from IGN
-// const val MAP_BASE_URL = "https://data.geopf.fr/annexes/ressources/vectorTiles/styles/PLAN.IGN/standard.json"
 // Tchap: Tiles from geo.data.gouv.fr
-// const val MAP_BASE_URL = "https://openmaptiles.geo.data.gouv.fr/styles/osm-bright/style.json"
-// Tchap: Generate and load map on device
-// const val MAP_BASE_URL = "https://api.maptiler.com/maps/streets/style.json"
-// const val STATIC_MAP_BASE_URL = "https://api.maptiler.com/maps/basic/static/"
-const val DEFAULT_PIN_ID = "DEFAULT_PIN_ID"
-
-const val INITIAL_MAP_ZOOM_IN_PREVIEW = 15.0
-const val INITIAL_MAP_ZOOM_IN_TIMELINE = 17.0
+const val MAP_BASE_URL = "https://openmaptiles.geo.data.gouv.fr/styles/osm-bright/style.json"
