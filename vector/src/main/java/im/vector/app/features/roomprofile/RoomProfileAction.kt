867f3517477e0675e15495966936e12055cbdfe4
yostyle <y.pintas@gmail.com>
2025-02-19 21:16:18 +0100

    Fix lint

--- vector/src/main/java/im/vector/app/features/roomprofile/RoomProfileAction.kt
+++ vector/src/main/java/im/vector/app/features/roomprofile/RoomProfileAction.kt
@@ -17,6 +17,7 @@ sealed class RoomProfileAction : VectorViewModelAction {
     object ShareRoomProfile : RoomProfileAction()
     object CreateShortcut : RoomProfileAction()
     object RestoreEncryptionState : RoomProfileAction()
+
     // TCHAP force to false to deactivate "Never send messages to unverified devices in room"
 //    data class SetEncryptToVerifiedDeviceOnly(val enabled: Boolean) : RoomProfileAction()
     data class ReportRoom(val reason: String) : RoomProfileAction()
